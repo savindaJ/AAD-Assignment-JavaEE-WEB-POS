779d40ce15d628ceef97b20584b2cf263068a298
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-01-08 11:16:19 +0530

    add configuraion

---
+++ Backend/src/main/java/lk/ijse/webPos/controller/sds.java
@@ -0,0 +1,9 @@
+package lk.ijse.webPos.controller;
+
+/**
+ * @author : savindaJ
+ * @date : 1/8/2024
+ * @since : 0.1.0
+ **/
+public class sds {
+}
---
+++ Backend/src/main/java/lk/ijse/webPos/controller/servlet/CustomerServlet.java
@@ -0,0 +1,22 @@
+package lk.ijse.webPos.controller.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author : savindaJ
+ * @date : 1/8/2024
+ * @since : 0.1.0
+ **/
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
