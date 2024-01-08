c5db1127f158ab98519df656f6a83f8693e0607a
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-01-08 11:28:20 +0530

    add filter

---
+++ Backend/src/main/java/lk/ijse/webPos/controller/filter/DefaultFilter.java
@@ -0,0 +1,35 @@
+package lk.ijse.webPos.controller.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author : savindaJ
+ * @date : 1/8/2024
+ * @since : 0.1.0
+ **/
+@WebFilter(urlPatterns = "/*")
+public class DefaultFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Init Filter !");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest, servletResponse);
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods", "DELETE,PUT");
+        resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
+        resp.setContentType("application/json");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Destroyed Filter !");
+    }
+}
