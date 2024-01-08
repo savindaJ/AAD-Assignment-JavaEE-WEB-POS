b689237e970693dad1cc0d957c650084fbac77c1
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-01-08 11:55:33 +0530

    add bo implementaion

--- Backend/src/main/java/lk/ijse/webPos/bo/custom/CustomerBO.java
+++ Backend/src/main/java/lk/ijse/webPos/bo/custom/CustomerBO.java
@@ -1,9 +1,11 @@
 package lk.ijse.webPos.bo.custom;
 
+import lk.ijse.webPos.bo.SuperBO;
+
 /**
  * @author : savindaJ
  * @date : 1/8/2024
  * @since : 0.1.0
  **/
-public interface CustomerBO {
+public interface CustomerBO extends SuperBO {
 }
--- Backend/src/main/java/lk/ijse/webPos/controller/servlet/CustomerServlet.java
+++ Backend/src/main/java/lk/ijse/webPos/controller/servlet/CustomerServlet.java
@@ -1,10 +1,15 @@
 package lk.ijse.webPos.controller.servlet;
 
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.webPos.bo.BOFactory;
+import lk.ijse.webPos.bo.custom.CustomerBO;
 
 import java.io.IOException;
 
@@ -15,8 +20,25 @@ import java.io.IOException;
  **/
 @WebServlet(urlPatterns = "/customer" ,loadOnStartup = 1)
 public class CustomerServlet extends HttpServlet {
+
+    private CustomerBO customerBO;
+
+    @Override
+    public void init() throws ServletException {
+        customerBO = BOFactory.getInstance().getBO(BOFactory.BOTypes.CUSTOMER);
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(customerBO);
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+        String id = jsonObject.getString("id");
+        System.out.println(id);
 
+        resp.getWriter().println("sample");*/
     }
 }
