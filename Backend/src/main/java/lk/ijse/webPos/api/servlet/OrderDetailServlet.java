3e74a7d6b5f2d0473d1fd4193fee43ebbda489cf
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-01-14 13:13:20 +0530

    add responce

--- Backend/src/main/java/lk/ijse/webPos/api/servlet/OrderDetailServlet.java
+++ Backend/src/main/java/lk/ijse/webPos/api/servlet/OrderDetailServlet.java
@@ -32,9 +32,8 @@ public class OrderDetailServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ArrayList<OrderDetailDTO> list = orderDetailBO.getAllOrderDetails();
-//        resp.getWriter().write(new RespMessage<OrderDetailDTO> ().createMassage("200", "Success", list));
-        for (OrderDetailDTO orderDetailDTO : list) {
-            System.out.println(orderDetailDTO);
-        }
+        resp.setContentType("application/json");
+        resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+        resp.getWriter().write(new RespMessage<OrderDetailDTO> ().createMassage("200", "Success", list));
     }
 }
