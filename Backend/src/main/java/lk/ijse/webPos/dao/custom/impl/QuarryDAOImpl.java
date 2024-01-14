4d8de2e9548b371ee64e05387ad6a00efd3a7ba3
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-01-14 12:05:05 +0530

    add quarry dao impl

---
+++ Backend/src/main/java/lk/ijse/webPos/dao/custom/impl/QuarryDAOImpl.java
@@ -0,0 +1,12 @@
+package lk.ijse.webPos.dao.custom.impl;
+
+import lk.ijse.webPos.dao.custom.QuarryDAO;
+
+/**
+ * @author : savindaJ
+ * @date : 1/14/2024
+ * @since : 0.1.0
+ **/
+public class QuarryDAOImpl implements QuarryDAO {
+
+}
