c1a88e8cf9f5fb35e2fdad34158e7a7ea5762053
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-01-14 12:04:16 +0530

    add quarry dao

---
+++ Backend/src/main/java/lk/ijse/webPos/dao/custom/QuarryDAO.java
@@ -0,0 +1,11 @@
+package lk.ijse.webPos.dao.custom;
+
+import lk.ijse.webPos.dao.SuperDAO;
+
+/**
+ * @author : savindaJ
+ * @date : 1/14/2024
+ * @since : 0.1.0
+ **/
+public interface QuarryDAO extends SuperDAO {
+}
