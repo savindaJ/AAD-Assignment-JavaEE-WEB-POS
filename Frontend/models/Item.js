ee52b4c3bd67f22ad2eb4d30214eb922574e914a
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-01-15 10:49:23 +0530

    modify dependency

--- Frontend/models/Item.js
+++ Frontend/models/Item.js
@@ -4,3 +4,8 @@ let item={
     qtyOnHand:0,
     unitPrice:0
 }
+
+let customer = new Customer("C001","Kasun","Galle",25000);
+console.log(customer.getAddress());
+let json = JSON.stringify(customer);
+console.log(json);
