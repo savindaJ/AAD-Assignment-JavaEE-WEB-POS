494025d36ed8ac74e16171e9fd4d70e89686cbd1
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-01-20 15:19:37 +0530

    add alert !

--- Frontend/controllers/registerController.js
+++ Frontend/controllers/registerController.js
@@ -20,7 +20,28 @@ $('#btn-signUp').on('click', function () {
         type: 'post',
         data:JSON.stringify(user),
         success:function (res) {
-            console.log(res);
+            let timerInterval;
+            Swal.fire({
+                title: "Send Redirect Request, Please Wait...",
+                html: "I will close in <b></b> milliseconds.",
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading();
+                    const timer = Swal.getPopup().querySelector("b");
+                    timerInterval = setInterval(() => {
+                        timer.textContent = `${Swal.getTimerLeft()}`;
+                    }, 100);
+                },
+                willClose: () => {
+                    clearInterval(timerInterval);
+                }
+            }).then((result) => {
+                /* Read more about handling dismissals below */
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    window.location.href = "../pages/main.html";
+                }
+            });
         },
         error:function (err) {
             console.log(err);
