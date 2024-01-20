13de304eb2b33418c0568d383f1cac32993e00d4
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-01-20 10:13:25 +0530

    add components

--- Frontend/db/db.js
+++ Frontend/db/db.js
@@ -1,14 +1,2 @@
-var customerDB = [];
-var itemDB = [];
-var orderDB = [
-    /*{
-        oid: "OID-001",
-        date: "2023-10-06",
-        customerID: "C00-001",
-        orderDetails: [
-            {oid: "OID-001", code: "I00-001", qty: 10, unitPrice: 145.00},
-            {oid: "OID-001", code: "I00-002", qty: 2, unitPrice: 345.00}
-        ]
-    }*/
-];
 let baseUrl ="http://192.168.80.174:8080/app/";
+let itemDB;
