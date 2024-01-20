072b5fa56a19d11dbfde4fa23858d9d80cf1e407
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-01-20 12:14:03 +0530

    add user servlet

---
+++ Backend/src/main/java/lk/ijse/webPos/api/servlet/UserServlet.java
@@ -0,0 +1,33 @@
+package lk.ijse.webPos.api.servlet;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author : savindaJ
+ * @date : 1/20/2024
+ * @since : 0.1.0
+ **/
+@WebServlet(urlPatterns = "/user")
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("do post");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("do get");
+        System.out.println("do post");
+        Jsonb jsonb = JsonbBuilder.create();
+        String json = jsonb.toJson(req.getReader(), Object.class);
+        System.out.println(json);
+    }
+}
--- Frontend/controllers/registerController.js
+++ Frontend/controllers/registerController.js
@@ -2,7 +2,25 @@ $("#txt-email,#txt-fname,#txt-lname,#txt-pw").on("keyup", function () {
     setBtn();
 });
 
+$('#btn-signUp').on('click', function () {
+    let email = $('#txt-email').val();
+    let fname = $('#txt-fname').val();
+    let lname = $('#txt-lname').val();
+    let pw = $('#txt-pw').val();
 
+    const user = {
+        email: email,
+        fname: fname,
+        lname: lname,
+        pw: pw
+    }
+
+    $.ajax({
+        url: 'http://localhost:8080/app/user',
+        type: 'get'
+    });
+
+});
 
 $('#hyp-login').on('click', function () {
     window.location.href = "../index.html";
