2e5034294b25bc567b0027c3f5231e9611fa1826
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-01-21 10:12:59 +0530

    add componets

--- Frontend/controllers/loginController.js
+++ Frontend/controllers/loginController.js
@@ -39,7 +39,21 @@ $('#btn-sign-in').on('click', function () {
             });
         },
         error:function (err) {
-            alert('error !')
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "error",
+                title: 'invalid username or password !'
+            });
         }
     });
 });
